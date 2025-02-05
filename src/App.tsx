8973b29ac1bd9050141d7550d32e0a50c30f2e3f
Renan Castro <renandf@gmail.com>
2025-02-05 18:58:36 +1000

    feat: Add Routes and global theme variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import { Router } from './Router'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
 
   return (
-    <h1>Hellow world</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      
+      <BrowserRouter >
+        
+          <Router />
+        
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
